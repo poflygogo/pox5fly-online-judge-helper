3a6da41aabd80b2fd2b88d08032061de3e5f6b58
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-12-13 23:34:33 +0800

    doc: config the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pox5fly-oj",
-    version="0.1.0",
+    version="0.1.1",
     description="A local utility package for Online Judge testing",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
